e097656e51776eba022f304f51e02a74ea7adc70
Anton Tetov <anton@tetov.xyz>
2021-11-11 18:52:41 +0100

    update

---
+++ src/bf3dp_fab/__main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys
+import pkg_resources
+import compas
+
+
+if __name__ == '__main__':
+
+    print()
+    print('Yay! bf3dp_fab is installed correctly!')
+    print()
+    print('bf3dp_fab: {}'.format(compas.__version__))
+    print('Python: {}'.format(str(sys.version)))
