47aec17701e1f9d9debcf24aea93103c38e80ac0
Anton Tetov <anton@tetov.se>
2023-06-14 12:52:04 +0200

    build: update build and deps

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from setuptools import setup
 
-setup(
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-)
+setup()
