ba05d64bf72112a133b3b7fd5a8c2303db0e1a56
Anton Tetov <anton@tetov.se>
2023-06-14 12:53:36 +0200

    style: ruff changes

--- src/bf3dp_fab/__main__.py
+++ src/bf3dp_fab/__main__.py
@@ -2,14 +2,12 @@
 from __future__ import print_function
 
 import sys
-import pkg_resources
 import compas
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     print()
-    print('Yay! bf3dp_fab is installed correctly!')
+    print("Yay! bf3dp_fab is installed correctly!")
     print()
-    print('bf3dp_fab: {}'.format(compas.__version__))
-    print('Python: {}'.format(str(sys.version)))
+    print("bf3dp_fab: {}".format(compas.__version__))
+    print("Python: {}".format(str(sys.version)))
--- src/compas_bf3dp_fab_plugin/__init__.py
+++ src/compas_bf3dp_fab_plugin/__init__.py
@@ -1,3 +1,6 @@
-"""This package is set up to be discovered by compas pluggable rhino_installable_package."""
+"""compas_rhino plugin
+
+This package is set up to be discovered by compas pluggable
+rhino_installable_package."""
 
 __all_plugins__ = ["bf3dp_fab.rhino_install"]
